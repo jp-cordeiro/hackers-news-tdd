f1598494ccc9532351c30c10b881ee7acbfa8103
jp-cordeiro <jpc.araujo@hotmail.com>
2020-08-23 22:18:33 -0300

    add test of initializes with 0% width

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -6,4 +6,9 @@ describe('ProgressBar.vue', () => {
     const wrapper = shallowMount(ProgressBar);
     expect(wrapper.classes()).toContain('hidden');
   });
+
+  test('initializes with 0% width', () => {
+    const wrapper = shallowMount(ProgressBar);
+    expect(wrapper.element.style.width).toBe('0%');
+  });
 });
