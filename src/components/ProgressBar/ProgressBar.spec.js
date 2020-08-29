f041c579c7e73408a0d858b2f2ba29e7bf69f330
JP <jp@home.com>
2020-08-29 19:50:33 -0300

    sets the bar to 100% width when finish is called and hides the bar when finish is called

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -23,4 +23,24 @@ describe('ProgressBar.vue', () => {
 
     expect(wrapper.classes()).not.toContain('hidden');
   });
+
+  test('sets the bar to 100% width when finish is called', async () => {
+    wrapper.vm.start();
+    await wrapper.vm.$nextTick();
+
+    wrapper.vm.finish();
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.element.style.width).toBe('100%');
+  });
+
+  test('hides the bar when finish is called', async () => {
+    wrapper.vm.start();
+    await wrapper.vm.$nextTick();
+
+    wrapper.vm.finish();
+    await wrapper.vm.$nextTick();
+
+    expect(wrapper.classes()).toContain('hidden');
+  });
 });
