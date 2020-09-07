1b251bb76f0c045d395628c07077cc616857897f
JP <jp@home.com>
2020-09-06 23:29:55 -0300

    increases width by 1% every 100ms after start call and clears timer when finish is called

--- src/components/ProgressBar/ProgressBar.spec.js
+++ src/components/ProgressBar/ProgressBar.spec.js
@@ -3,8 +3,11 @@ import ProgressBar from './ProgressBar.vue';
 
 describe('ProgressBar.vue', () => {
   let wrapper;
+  let nextTick;
   beforeEach(() => {
     wrapper = shallowMount(ProgressBar);
+    nextTick = async () => wrapper.vm.$nextTick();
+    jest.useFakeTimers();
   });
 
   test('is hidden on initial render', () => {
@@ -19,38 +22,66 @@ describe('ProgressBar.vue', () => {
     expect(wrapper.classes()).toContain('hidden');
 
     wrapper.vm.start();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     expect(wrapper.classes()).not.toContain('hidden');
   });
 
   test('sets the bar to 100% width when finish is called', async () => {
     wrapper.vm.start();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     wrapper.vm.finish();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     expect(wrapper.element.style.width).toBe('100%');
   });
 
   test('hides the bar when finish is called', async () => {
     wrapper.vm.start();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     wrapper.vm.finish();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     expect(wrapper.classes()).toContain('hidden');
   });
 
   test('resets to 0% width when start is called', async () => {
     wrapper.vm.finish();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     wrapper.vm.start();
-    await wrapper.vm.$nextTick();
+    await nextTick();
 
     expect(wrapper.element.style.width).toBe('0%');
+  });
+
+  test('increases width by 1% every 100ms after start call', async () => {
+    wrapper.vm.start();
+    jest.runTimersToTime(100);
+    await nextTick();
+    expect(wrapper.element.style.width).toBe('1%');
+
+    jest.runTimersToTime(900);
+    await nextTick();
+    expect(wrapper.element.style.width).toBe('10%');
+
+    jest.runTimersToTime(4000);
+    await nextTick();
+    expect(wrapper.element.style.width).toBe('50%');
+  });
+
+  test('clears timer when finish is called', async () => {
+    jest.spyOn(window, 'clearInterval');
+    setInterval.mockReturnValue(123);
+
+    wrapper.vm.start();
+    await nextTick();
+
+    wrapper.vm.finish();
+    await nextTick();
+
+    expect(window.clearInterval).toHaveBeenCalledWith(123);
   });
 });
